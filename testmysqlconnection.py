5fdeb56641954d6b5ccb203f6457f53ad18c4e07
sumitsingh-kr <sumokrsingh@gmail.com>
2022-04-20 13:34:30 +0530

    minor changes

---
+++ testmysqlconnection.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import mysqlconnection
+
+class testconection(unittest.TestCase):
+
+    def test_check_base_dir_config_file(self):
+        actual_result = mysqlconnection.BASE_DIR+"config.json"
+
+        expected_result = os.getcwd()+'config.json'
+        self.assertEqual(expected_result,actual_result)
+
+    
+
+
+
+        
+
+
+
+
+
+
